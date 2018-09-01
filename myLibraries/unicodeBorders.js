7c8568fd0dce8f24a79db65bb60d0ffeaed51c3c
evb0110 <7149553@gmail.com>
2018-09-01 12:12:57 +0300

    newcommit

--- myLibraries/unicodeBorders.js
+++ myLibraries/unicodeBorders.js
@@ -1,5 +1,5 @@
 const border = "(^|(?<=[\\p{L}\\p{M}])(?=[^\\p{L}\\p{M}])|(?<=[^\\p{L}\\p{M}])(?=[\\p{L}\\p{M}])|$)";
-const letter = "\\p{L}\\p{M}*";
+const letter = "(?:\\p{L}\\p{M}*)";
 
 function toUnicode( str ){
   str = str.replace( /\\w/g, letter);
