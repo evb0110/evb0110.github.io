b2cf52a261acab1b32adcb075fdeebab72e8b0d8
evb0110 <7149553@gmail.com>
2018-09-03 13:27:26 +0300

    newcommit

---
+++ Maalula-testing/myLibraries/unicodeBorders.js
@@ -0,0 +1,11 @@
+const border = "(^|(?<=[\\p{L}\\p{M}])(?=[^\\p{L}\\p{M}])|(?<=[^\\p{L}\\p{M}])(?=[\\p{L}\\p{M}])|$)";
+const letter = "(?:\\p{L}\\p{M}*)";
+
+function toUnicode( str ){
+  str = str.replace( /\\w/g, letter);
+  str = str.replace( /\\b/g, border);
+  return str;
+}
+
+
+
