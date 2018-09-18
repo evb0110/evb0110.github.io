8fb13ec30314b2c31f4d4c7471789bfe0b9a424c
evb0110 <7149553@gmail.com>
2018-09-18 14:13:32 +0300

    newcommit

---
+++ myLibraries/copyToClip.js
@@ -0,0 +1,11 @@
+function copyToClip(str) {
+  function listener(e) {
+    e.clipboardData.setData("text/html", str);
+    e.clipboardData.setData("text/plain", str);
+    e.preventDefault();
+  }
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+};
+ 
