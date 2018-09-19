bcbfb39a023b287a605f33d78696f5989859c7cc
evb0110 <7149553@gmail.com>
2018-09-19 15:45:44 +0300

    newcommit

--- myLibraries/copyToClip.js
+++ myLibraries/copyToClip.js
@@ -1,7 +1,15 @@
 function copyToClip(str) {
   function listener(e) {
     e.clipboardData.setData("text/html",  str);
-    e.clipboardData.setData("text/plain", str);
+    let parser = new DOMParser();
+    let text = parser
+       .parseFromString(str, 'text/html')
+       .documentElement
+       .textContent
+       ;
+    text = text.replace(/\n\n\n/gm, "");
+    text = text.replace(/^\s*\n/, "");
+    e.clipboardData.setData("text/plain", text);
     e.preventDefault();
   }
   document.addEventListener("copy", listener);
@@ -14,5 +22,8 @@ function copyToClip(str) {
 https://stackoverflow.com/questions/23934656/javascript-copy-rich-text-contents-to-clipboard
 
 https://jsfiddle.net/jdhenckel/km7prgv4/3/
+
+a better solution:
+https://stackoverflow.com/questions/34191780/javascript-copy-string-to-clipboard-as-text-html
 
 */
