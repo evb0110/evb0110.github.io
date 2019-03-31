0f5ddd20a982a6db5c16147614a6d9a02f6d5317
evb0110 <7149553@gmail.com>
2019-03-31 19:28:49 +0300

    reverted header none

--- Maalula/mwa_corpus_search/app.js
+++ Maalula/mwa_corpus_search/app.js
@@ -32,11 +32,11 @@ function handleShow(event) {
 
   if (Number.isNaN(volumeNumber)) {
     emptyElement(outer2);
-    return outer2.textContent = 'volumeNumber should be a valid number';
+    return (outer2.textContent = 'volumeNumber should be a valid number');
   }
   if (Number.isNaN(textNumber)) {
     emptyElement(outer2);
-    return outer2.textContent = 'textNumber should be a valid number';
+    return (outer2.textContent = 'textNumber should be a valid number');
   }
 
   produceText(volumeNumber, textNumber);
@@ -52,15 +52,38 @@ const pagetitle = document.querySelector('.pagetitle');
 
 let prevScrollpos;
 function hideHeader(event) {
-  const currentScrollPos = event.target.scrollTop;
-  if (prevScrollpos > currentScrollPos) {
-    pagetitle.classList.add('block');
-    pagetitle.classList.remove('none');
-  } else {
+  // const currentScrollPos = event.target.scrollTop;
+  // if (prevScrollpos > currentScrollPos) {
+  //   pagetitle.classList.add('block');
+  //   pagetitle.classList.remove('none');
+  // } else {
+  //   pagetitle.classList.add('none');
+  //   pagetitle.classList.remove('block');
+  // }
+  // prevScrollpos = currentScrollPos;
+  if ([outer1, outer2].map(c => c.scrollTop).some(x => x > 0)) {
     pagetitle.classList.add('none');
     pagetitle.classList.remove('block');
+  } else {
+    pagetitle.classList.add('block');
+    pagetitle.classList.remove('none');
   }
-  prevScrollpos = currentScrollPos;
 }
+
+// const copyButton = document.querySelector('.copy_button');
+// copyButton.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   copyToClip();
+// });
+// function copyToClip(str = outer1.innerHTML) {
+//   function listener(e) {
+//     e.preventDefault();
+//     e.clipboardData.setData('text/html', str);
+//     e.clipboardData.setData('text/plain', str);
+//   }
+//   document.addEventListener('copy', listener);
+//   document.execCommand('copy');
+//   document.removeEventListener('copy', listener);
+// }
 
 export { outer1, outer2, emptyElement, volumeNumberBox, textNumberBox };
