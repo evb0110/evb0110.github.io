69399b3e6c5aa740e55dbb130b300d37be7535f6
Eugene Barsky <7149553@gmail.com>
2020-09-21 10:05:26 +0300

    turoyo search new

---
+++ Turoyo/turoyo_corpus_search_new/js/about.b09c99a7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},u=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About Page")])])}],s=n("2877"),c={},l=Object(s["a"])(c,a,u,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=about.b09c99a7.js.map
