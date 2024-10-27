f9f26da70fcba78aa4d4a889d610e92b430e892f
Eugene Barsky <7149553@gmail.com>
2024-10-27 21:10:19 +0300

    newsite3

--- aramaicsite/assets/MaalulaRouter-CmPAxKuI.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,A as r,o as t,z as c}from"./index-DuNLDwPv.js";const n={};function a(s,_){const e=c("router-view");return t(),r(e)}const f=o(n,[["render",a]]);export{f as default};
--- aramaicsite/assets/UrmiRouter-Bdq38xX5.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,A as r,o as t,z as c}from"./index-DuNLDwPv.js";const n={};function s(_,a){const e=c("router-view");return t(),r(e)}const i=o(n,[["render",s]]);export{i as default};
