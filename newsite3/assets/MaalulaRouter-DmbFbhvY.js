2a3b8805e5c56455eac3ad57975990230809c120
Eugene Barsky <7149553@gmail.com>
2024-10-27 15:48:51 +0300

    newsite3

---
+++ newsite3/assets/MaalulaRouter-DmbFbhvY.js
@@ -0,0 +1,1 @@
+import{_ as o,v as r,o as t,s as c}from"./index-CcY6XDHE.js";const n={};function s(a,_){const e=c("router-view");return t(),r(e)}const f=o(n,[["render",s]]);export{f as default};
---
+++ newsite3/assets/TuroyoRouter-WthFf1_w.js
@@ -0,0 +1,1 @@
+import{_ as e,v as r,o as t,s as c}from"./index-CcY6XDHE.js";const n={};function s(_,a){const o=c("router-view");return t(),r(o)}const f=e(n,[["render",s]]);export{f as default};
