e7ab6cc4cd5d65bbe6db2ca13ddea491834a2442
Eugene Barsky <7149553@gmail.com>
2024-10-28 11:21:28 +0300

    newsite3

---
+++ aramaicsite/assets/UrmiLegacy-BEAj6XHb.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-DSUiMQq4.js";const r={};function c(t,n){return" Redirecting to Urmi legacy site "}const i=e(r,[["render",c]]);export{i as default};
--- aramaicsite/assets/UrmiLegacy-CqacxlxL.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-BMVER-wz.js";const r={};function c(t,n){return" Redirecting to Urmi legacy site "}const i=e(r,[["render",c]]);export{i as default};
