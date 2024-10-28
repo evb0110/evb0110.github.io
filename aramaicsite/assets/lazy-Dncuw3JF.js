50ddc33883011f1d65bf4e78115c732c7d3b1d01
Eugene Barsky <7149553@gmail.com>
2024-10-28 16:16:08 +0300

    newsite3

--- aramaicsite/assets/lazy-Dncuw3JF.js
+++
@@ -1,1 +0,0 @@
-import{p as n,W as r,c as u,X as l}from"./index-D5nMvJfZ.js";const f=n({eager:Boolean},"lazy");function m(e,o){const a=r(!1),s=u(()=>a.value||e.eager||o.value);l(o,()=>a.value=!0);function t(){e.eager||(a.value=!1)}return{isBooted:a,hasContent:s,onAfterLeave:t}}export{f as m,m as u};
