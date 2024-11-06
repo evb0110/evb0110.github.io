22a64228534db9055ff339ed233229084530ad43
Eugene Barsky <7149553@gmail.com>
2024-11-06 12:30:06 +0300

    newsite3

--- aramaicsite/assets/SyriacRouter-DoBOMq5E.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,D as r,o as t,C as c}from"./index-B2MtYJlS.js";const n={};function s(_,a){const e=c("router-view");return t(),r(e)}const i=o(n,[["render",s]]);export{i as default};
---
+++ aramaicsite/assets/UrmiLegacy-Ba49ptwn.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-CeXCJhcw.js";const r={};function c(t,n){return" Redirecting to Urmi legacy site "}const i=e(r,[["render",c]]);export{i as default};
