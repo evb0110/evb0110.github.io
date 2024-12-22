a7712ba98bd5fc8bc2abc7aa4f1638e9f45d45f1
Eugene Barsky <7149553@gmail.com>
2024-12-22 18:22:33 +0300

    newsite3

---
+++ aramaicsite/assets/TuroyoRouter-CmrCHHdx.js
@@ -0,0 +1,1 @@
+import{_ as e,C as r,o as t,D as c}from"./index-Dk7H_d14.js";const n={};function s(_,a){const o=r("router-view");return t(),c(o)}const f=e(n,[["render",s]]);export{f as default};
---
+++ aramaicsite/assets/UrmiLegacy-FzKcWMVv.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-Dk7H_d14.js";const r={};function c(t,n){return" Redirecting to Urmi legacy site "}const i=e(r,[["render",c]]);export{i as default};
