8d285bf8ce77cf99b550d31ebbfe899b3335b549
Eugene Barsky <7149553@gmail.com>
2025-01-02 11:47:53 +0300

    newsite3

---
+++ aramaicsite/assets/lazy-A4pNVVBF.js
@@ -0,0 +1,1 @@
+import{U as n,c as r,e as u,v as l}from"./index-CdJUwpE1.js";const f=l({eager:Boolean},"lazy");function m(a,o){const e=n(!1),s=r(()=>e.value||a.eager||o.value);u(o,()=>e.value=!0);function t(){a.eager||(e.value=!1)}return{isBooted:e,hasContent:s,onAfterLeave:t}}export{f as m,m as u};
