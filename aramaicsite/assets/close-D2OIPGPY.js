2319393cf775266d1c469ad7b623fad2649b18b3
Eugene Barsky <7149553@gmail.com>
2025-01-25 22:20:00 +0300

    newsite3

--- aramaicsite/assets/close-D2OIPGPY.js
+++
@@ -1,1 +0,0 @@
-import{o,f as t,n as r}from"./index-DMQf1O_k.js";const n={xmlns:"http://www.w3.org/2000/svg",width:"24",height:"24",fill:"none",stroke:"currentColor","stroke-linecap":"round","stroke-linejoin":"round","stroke-width":"2"};function s(l,e){return o(),t("svg",n,e[0]||(e[0]=[r("path",{d:"M18 6 6 18M6 6l12 12"},null,-1)]))}const i={render:s};export{i as default,s as render};
