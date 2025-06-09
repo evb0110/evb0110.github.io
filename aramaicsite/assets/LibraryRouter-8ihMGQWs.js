a8c268c982d1d2938d0b4e33f6d02d8cb4236d31
Eugene Barsky <7149553@gmail.com>
2025-06-09 14:57:48 +0300

    newsite3

--- aramaicsite/assets/LibraryRouter-8ihMGQWs.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as r,v as t,s as c}from"./index-Cj5KEw6U.js";const n={};function s(_,a){const e=c("router-view");return r(),t(e)}const i=o(n,[["render",s]]);export{i as default};
--- aramaicsite/assets/MaalulaRouter-C91ixrTn.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,s as r,o as t,v as c}from"./index-Cj5KEw6U.js";const n={};function s(a,_){const e=r("router-view");return t(),c(e)}const f=o(n,[["render",s]]);export{f as default};
