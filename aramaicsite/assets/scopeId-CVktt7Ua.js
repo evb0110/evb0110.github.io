83b4deb01904b47b0858e6d977d42d74c2301c54
Eugene Barsky <7149553@gmail.com>
2025-06-09 15:59:28 +0300

    newsite3

--- aramaicsite/assets/scopeId-CVktt7Ua.js
+++
@@ -1,1 +0,0 @@
-import{ah as t,c,e as u,C as r,be as d}from"./index-Csr_zljy.js";const l=r({eager:Boolean},"lazy");function p(o,e){const a=t(!1),s=c(()=>a.value||o.eager||e.value);u(e,()=>a.value=!0);function n(){o.eager||(a.value=!1)}return{isBooted:a,hasContent:s,onAfterLeave:n}}function m(){const e=d("useScopeId").vnode.scopeId;return{scopeId:e?{[e]:""}:void 0}}export{m as a,l as m,p as u};
